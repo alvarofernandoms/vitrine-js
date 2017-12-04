a5933dcf2bd6f09c7cb4f2be952ca35577f5fde0
Álvaro Fernando <alvarofernandoms@gmail.com>
2017-12-04 12:32:17 -0200

    Add panel title and init visited item

--- js/app/Vitrine.js
+++ js/app/Vitrine.js
@@ -15,21 +15,39 @@ class Vitrine {
       'http://roberval.chaordicsystems.com/challenge/challenge.json');
     document.body.appendChild(scriptEl);
     window.X = function(data) {
-      let elements = [];
+      let elements = [],
+        elementVisited = [],
+        visistedItem = data.data.reference.item;
+
+      // Visisted item
+      elementVisited.push(`
+        <div class="recommendation-item">
+          <div class="recommendation-image">
+            <img src="http:${visistedItem.imageName}">
+          </div>
+          <p class="recommendation-name">${visistedItem.name}</p>
+          ${visistedItem.oldPrice ? '<p class="recommendation-old-price">De: ' + visistedItem.oldPrice : ''}
+          <p class="recommendation-price">Por: <span class="price-highlight">${visistedItem.price}</span></p>
+          <p class="recommendation-paymant-conditions">${visistedItem.productInfo.paymentConditions} sem juros</p>
+        </div>
+      `);
+
+      // Recommedations
       data.data.recommendation.map(recommendation => {
         elements.push(`
-          <div class="recommendation-item">
-            <div class="recommendation-image">
-              <img src="http:${recommendation.imageName}">
-            </div>
-            <p class="recommendation-name">${recommendation.name}</p>
-            ${recommendation.oldPrice ? '<p class="recommendation-old-price">De: ' + recommendation.oldPrice : ''}
-            <p class="recommendation-price">Por: <span class="price-highlight">${recommendation.price}</span></p>
-            <p class="recommendation-paymant-conditions">${recommendation.productInfo.paymentConditions} sem juros</p>
-          </div>
+        <div class="recommendation-item">
+        <div class="recommendation-image">
+        <img src="http:${recommendation.imageName}">
+        </div>
+        <p class="recommendation-name">${recommendation.name}</p>
+        ${recommendation.oldPrice ? '<p class="recommendation-old-price">De: ' + recommendation.oldPrice : ''}
+        <p class="recommendation-price">Por: <span class="price-highlight">${recommendation.price}</span></p>
+        <p class="recommendation-paymant-conditions">${recommendation.productInfo.paymentConditions} sem juros</p>
+        </div>
         `);
       });
       $('#vitrine-view .recommendation-panel').innerHTML = elements.join('');
+    //$('#vitrine-view .visited-item').innerHTML = elementVisited;
     };
   }
 
